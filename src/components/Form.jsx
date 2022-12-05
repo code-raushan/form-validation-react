25f995441089c3882f02c8307f0c50ab6a0003f8
Raushan Kumar <raushan.development@gmail.com>
2022-12-05 18:49:50 +0530

    added form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import './Form.css'
+import {useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import './Form.css';
 
 const Form = ()=>{
+    const schema = yup.object().shape({
+        fullName: yup.string().required("full name is required"),
+        email: yup.string().email().required("email is required"),
+        age: yup.number().positive().integer().min(18).required("age is required"),
+        password: yup.string().min(8).max(16).required("password is required"),
+        confirmPassword: yup.string().oneOf([yup.ref('password'), null]).required("enter correct password")
+    })
+    const {register, handleSubmit, formState:{ errors },} = useForm({
+        resolver: yupResolver(schema)
+    });
+    const onSubmit = (data)=>{
+        console.log(data);
+    }
     return (
-        <form>
-            <input type="text" placeholder='Full Name'/>
-            <input type="text" placeholder='Your Email'/>
-            <input type="text" placeholder='Age'/>
-            <input type="password" placeholder='Enter your password' />
-            <input type="text"  placeholder='Confirm your password'/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder='Full Name' {...register("fullName")}/>
+            <p>{errors.fullName?.message}</p>
+            <input type="text" placeholder='Your Email' {...register('email')}/>
+            <input type="text" placeholder='Age' {...register('age')}/>
+            <input type="password" placeholder='Enter your password' {...register('password')}/>
+            <input type="text"  placeholder='Confirm your password' {...register('confirmPassword')}/>
             <input type="submit" />
         </form>
     );
